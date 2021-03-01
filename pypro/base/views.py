e7e274831d6c5243cf18ea17416e3d1c03235280
faleite <fabricio_2310@hotmail.com>
2021-03-01 18:29:13 +0000

    Instalado layout e utilizado tag static
    close #45

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render # noqa
+from django.http import HttpResponse  # noqa
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('<html><body>Olá Django</body></html>')
+    return render(request, 'base/home.html')
