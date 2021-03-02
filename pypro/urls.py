01462e84fb7b957fba18b2096e983f12100df8da
faleite <fabricio_2310@hotmail.com>
2021-03-02 15:31:50 +0000

    Feito encapsulamento das urls
    close #49

--- pypro/urls.py
+++ pypro/urls.py
@@ -17,13 +17,12 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
-from pypro.base.views import home
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', include('pypro.base.urls')),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
